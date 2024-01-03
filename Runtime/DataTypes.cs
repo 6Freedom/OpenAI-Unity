4d3933750d832866b20220155785f26d736b77bf
RemiDrouot <remi.drouot@6freedom.studio>
2024-01-03 11:10:45 +0100

    [DataType] fix logprobs type

--- Runtime/DataTypes.cs
+++ Runtime/DataTypes.cs
@@ -114,7 +114,7 @@ namespace OpenAI
         public ChatMessage Message { get; set; }
         public ChatMessage Delta { get; set; }
         public int? Index { get; set; }
-        public string Logprobs { get; set; }
+        public int? Logprobs { get; set; }
         public string FinishReason { get; set; }
     }
 
